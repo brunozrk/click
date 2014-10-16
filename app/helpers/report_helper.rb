bbeab2608cbb8926e4ead4e9667b3fe56e47da34
Bruno Zeraik <brunozrk@gmail.com>
2014-10-16 09:01:33 -0300

    improves hours report

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,22 +1,48 @@
 module ReportHelper
   def total(report)
-    first_entry = timeit(report.first_entry)
-    first_exit = timeit(report.first_exit)
-    second_entry = timeit(report.second_entry)
-    second_exit = timeit(report.second_exit)
-    Time.at((first_exit - first_entry) + (second_exit - second_entry)).gmtime.strftime('%H:%M')
+    hour_minute(worked(report))
   end
 
   def balance(report)
-    first_entry = timeit(report.first_entry)
-    first_exit = timeit(report.first_exit)
-    second_entry = timeit(report.second_entry)
-    second_exit = timeit(report.second_exit)
-    Time.at( 8.hour - ((first_exit - first_entry) + (second_exit - second_entry))).gmtime.strftime('%H:%M')
-    binding.pry
+    worked = worked(report)
+    if 8.hour > worked
+      "- #{hour_minute(8.hour - worked)}"
+    else
+      "+ #{hour_minute(worked - 8.hour)}"
+    end
   end
 
   private
+
+  def hour_minute(worked)
+    Time.at(worked).gmtime.strftime('%H:%M')
+  end
+
+  def worked(report)
+    first_entry, first_exit, second_entry, second_exit = report_values(report)
+
+    first_total = time_diff(first_entry, first_exit)
+    secont_total = time_diff(second_entry, second_exit)
+
+    first_total + secont_total
+  end
+
+  def time_diff(entry, exit)
+    if exit && entry
+      exit - entry
+    else
+      0
+    end
+  end
+
+  def report_values(report)
+    [
+      timeit(report.first_entry),
+      timeit(report.first_exit),
+      timeit(report.second_entry),
+      timeit(report.second_exit)
+    ]
+  end
 
   def timeit(value)
     Time.parse(value) unless value.empty?
