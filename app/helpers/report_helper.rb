bed9fe13dd7290c9dd3455891389cce9e3f9cd91
Bruno Zeraik <brunozrk@gmail.com>
2014-10-16 14:35:20 -0300

    use 'model' instead 'helper' to calculate

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,49 +1,13 @@
 module ReportHelper
-  def total(report)
-    hour_minute(worked(report))
-  end
-
-  def balance(report)
-    worked = worked(report)
-    if 8.hour > worked
-      "- #{hour_minute(8.hour - worked)}"
+  def sign(balance)
+    if balance[:sign]
+      "- #{hour_minute(balance[:time])}"
     else
-      "+ #{hour_minute(worked - 8.hour)}"
+      "+ #{hour_minute(balance[:time])}"
     end
   end
-
-  private
 
   def hour_minute(worked)
     Time.at(worked).gmtime.strftime('%H:%M')
-  end
-
-  def worked(report)
-    first_entry, first_exit, second_entry, second_exit, remote = report_values(report)
-    first_total = time_diff(first_entry, first_exit)
-    secont_total = time_diff(second_entry, second_exit)
-    first_total + secont_total + remote
-  end
-
-  def time_diff(entry, exit)
-    if exit && entry
-      exit - entry
-    else
-      0
-    end
-  end
-
-  def report_values(report)
-    [
-      timeit(report.first_entry),
-      timeit(report.first_exit),
-      timeit(report.second_entry),
-      timeit(report.second_exit),
-      timeit(report.remote).seconds_since_midnight
-    ]
-  end
-
-  def timeit(value)
-    Time.parse(value) unless value.empty?
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -6,4 +6,34 @@ class Report < ActiveRecord::Base
   validates :day, uniqueness: { scope: :user_id }
 
   default_scope { order('day DESC') }
+
+  def worked
+    first_total = time_diff(first_entry, first_exit)
+    second_total = time_diff(second_entry, second_exit)
+    first_total + second_total + timeit(remote).seconds_since_midnight
+  end
+
+  def balance
+    if 8.hour > worked
+      { time: 8.hour - worked, sign: true }
+    else
+      { time: worked - 8.hour, sign: false }
+    end
+  end
+
+  private
+
+  def time_diff(entry, exit)
+    entry = timeit(entry)
+    exit = timeit(exit)
+    if exit && entry
+      exit - entry
+    else
+      0
+    end
+  end
+
+  def timeit(value)
+    Time.parse(value) unless value.empty?
+  end
 end
