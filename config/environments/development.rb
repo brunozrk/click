7ea2ea21fabbd2f2e826eddb40868e0ad2d916de
Bruno Zeraik <brunozrk@gmail.com>
2014-10-17 15:58:31 -0300

    Sendgrid configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,14 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => '<username>',
+    :password       => '<password>',
+    :domain         => 'sendgrid.me'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
