d4ff7bd74525f626331383863f333439b2fe2b1f
Bruno Zeraik <brunozrk@gmail.com>
2014-11-09 17:17:48 -0200

    show estimated_exit in report list

--- spec/helpers/report_helper_spec.rb
+++ spec/helpers/report_helper_spec.rb
@@ -9,4 +9,23 @@ describe ReportHelper do
     it { expect(helper.hour_minute(8.hour)).to eq '08:00' }
     it { expect(helper.hour_minute(30.hour)).to eq '30:00' }
   end
+
+  describe '#second_exit' do
+    let(:report_without_second_exit) { FactoryGirl.build(:report_without_second_exit) }
+    let(:report) { FactoryGirl.build(:report) }
+
+    context 'when second_exit is NOT blank' do
+      it { expect(helper.second_exit(report)).to eq report.second_exit }
+    end
+
+    context 'when second_exit is blank' do
+      let(:expected) do
+        "<span title='Saída Estimada' class='text-muted'>
+          <i class='fa fa-fw fa-sign-out'></i>
+          18:30
+        </span>".squish
+      end
+      it { expect(helper.second_exit(report_without_second_exit).squish).to eq expected }
+    end
+  end
 end
