b966be0d12e2dc00b5ed168bd15a227b5dc3d7e5
Bruno Zeraik <brunozrk@gmail.com>
2014-12-01 14:36:06 -0200

    add update and destroy test to timetable controller

--- spec/controllers/timetables_controller_spec.rb
+++ spec/controllers/timetables_controller_spec.rb
@@ -66,4 +66,61 @@ describe TimetablesController do
       end
     end
   end
+
+  describe '#PUT update' do
+    let(:edit_timetable) { timetables(:timetable_1) }
+    let(:timetable_params) do
+      {
+        closing_day: Date.today + 10
+      }
+    end
+
+    let(:params) do
+      {
+        id: edit_timetable.id,
+        timetable: timetable_params
+      }
+    end
+
+    before do
+      allow(Timetable).to receive(:find).and_return(edit_timetable)
+    end
+
+    context 'with valid params' do
+      it 'update the remote field' do
+        expect do
+          put :update, params
+        end.to change(edit_timetable, :closing_day)
+      end
+
+      it 'redirect to timetable listing' do
+        put :update, params
+        expect(response).to redirect_to(timetables_path)
+      end
+    end
+  end
+
+  describe '#DELETE destroy' do
+    let(:deleted_timetable) { timetables(:timetable_1) }
+    let(:unallowed_timetable) { timetables(:unallowed_timetable) }
+
+    context 'when the current_user is the timetable owner' do
+      it 'remove the timetable' do
+        delete :destroy, id: deleted_timetable.id
+        expect(Timetable.where(id: deleted_timetable.id)).to be_empty
+      end
+
+      it 'redirect to timetable listing' do
+        delete :destroy, id: deleted_timetable.id
+        expect(response).to redirect_to(timetables_path)
+      end
+    end
+
+    context 'when the current_user is NOT the timetable owner' do
+      it 'not remove the timetable' do
+        delete :destroy, id: unallowed_timetable.id
+        expect(Timetable.where(id: unallowed_timetable.id)).to_not be_empty
+      end
+    end
+  end
 end
