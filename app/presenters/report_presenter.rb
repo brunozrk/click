7d7faef38b6ea4e5f38d59028c8b1738e1c02428
Felipe Gentil <cdigentil@gmail.com>
2015-06-20 09:54:36 -0300

    Refactored estimated exits in order to have one method only

--- app/presenters/report_presenter.rb
+++ app/presenters/report_presenter.rb
@@ -1,32 +1,12 @@
 require 'pry'
 class ReportPresenter < Burgundy::Item
 
-  def estimated_first_exit
-    return first_exit if first_exit.present?
-    if estimable?(0)
+  def estimated_time(index)
+    return index_exit(index) if index_exit(index).present?
+    if estimable?(index_to_integer(index))
       h.content_tag(:span, title: 'Saída Estimada', class: 'text-muted') do
         h.content_tag(:i, nil, class: 'fa fa-fw fa-sign-out') +
-          estimated_exit('first').strftime('%H:%M')
-      end
-    end
-  end
-
-  def estimated_second_exit
-    return second_exit if second_exit.present?
-    if estimable?(1)
-      h.content_tag(:span, title: 'Saída Estimada', class: 'text-muted') do
-        h.content_tag(:i, nil, class: 'fa fa-fw fa-sign-out') +
-          estimated_exit('second').strftime('%H:%M')
-      end
-    end
-  end
-
-  def estimated_third_exit
-    return third_exit if third_exit.present?
-    if estimable?(2)
-      h.content_tag(:span, title: 'Saída Estimada', class: 'text-muted') do
-        h.content_tag(:i, nil, class: 'fa fa-fw fa-sign-out') +
-          estimated_exit('third').strftime('%H:%M')
+          estimated_exit(index).strftime('%H:%M')
       end
     end
   end
@@ -46,6 +26,20 @@ class ReportPresenter < Burgundy::Item
   end
 
   private
+  def index_exit(index)
+    send("#{index}_exit".to_sym)
+  end
+
+  def index_to_integer(index)
+    case index
+    when :first
+      0
+    when :second
+      1
+    when :third
+      2
+    end
+  end
 
   def estimable?(entry)
     entries.at(entry).present? &&
@@ -58,8 +52,7 @@ class ReportPresenter < Burgundy::Item
     '(Dia não útil)'
   end
 
-  def estimated_exit entry
-    current_entry = "#{entry}_entry".to_sym
-    Time.parse(send(current_entry)) + balance.fetch(:time)
+  def estimated_exit(entry)
+    Time.parse(send("#{entry}_entry".to_sym)) + balance.fetch(:time)
   end
 end
