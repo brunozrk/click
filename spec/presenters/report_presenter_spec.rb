3fb9d9432f03c89f228011d3276de338dabf3e1c
Felipe Gentil <cdigentil@gmail.com>
2015-06-20 09:54:20 -0300

    Refactored specs to use estimated_second_exit

--- spec/presenters/report_presenter_spec.rb
+++ spec/presenters/report_presenter_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe ReportPresenter do
   let(:presenter) { described_class.new(report) }
 
-  describe '#last_exit' do
+  describe '#estimated_second_exit' do
     context 'when second_exit is NOT blank' do
       let(:report) { FactoryGirl.build(:report) }
-      it { expect(presenter.last_exit).to eq report.second_exit }
+      it { expect(presenter.estimated_second_exit).to eq report.second_exit }
     end
 
     context 'when second_exit is blank and estimated_exit exists' do
       let(:report) { FactoryGirl.build(:report_without_second_exit) }
-      it { expect(presenter.last_exit.squish).to include '18:30' }
+      it { expect(presenter.estimated_second_exit.squish).to include '18:30' }
     end
 
     context 'when second_exit and estimated_exit are blank' do
       let(:report) { FactoryGirl.build(:report_without_second_exit, second_entry: '') }
-      it { expect(presenter.last_exit).to eq nil }
+      it { expect(presenter.estimated_second_exit).to eq nil }
     end
   end
 
